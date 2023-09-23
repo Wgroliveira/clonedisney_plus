ff4659553c2fbe8dbd2126e6ccc28691b7598be3
Wgroliveira <129519069+Wgroliveira@users.noreply.github.com>
2023-09-23 14:05:16 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
+document.addEventListener('DOMContentLoaded', function(){
 const tabButtons = document.querySelectorAll('.shows__tabs__button');
+
+const heroSection = document.querySelector('.hero');
+const alturaHero = heroSection.clientHeight;
+
+window.addEventListener('scroll', function() {
+    const posicaoAtual = window.scrollY;
+
+    if (posicaoAtual < alturaHero) {
+        ocultaElementosDoHeader();
+    } else {
+        exibeElementosDoHeader();
+    }
+})
+
+//Programação das abas de atrações
 tabButtons.forEach(button => {
     button.addEventListener('click', () => {
         const tabId = button.getAttribute('data-tab-button');
@@ -15,3 +31,32 @@ tabButtons.forEach(button => {
         button.classList.add('shows__tabs__button--is--active');
     });
 });
+
+});
+
+//Seção FAQ, acordion
+    
+document.addEventListener('DOMContentLoaded', function() {
+const questions = document.querySelectorAll('.faq__questions__item');
+
+questions.forEach(question => {
+    const questionTitle = question.querySelector('.faq__questions__item__question');
+    const answer = question.querySelector('.faq__questions__item__answer');
+
+    questionTitle.addEventListener('click', () => {
+        question.classList.toggle('faq__questions__item--is-open');
+
+        answer.classList.toggle('hidden');
+    });
+});
+});
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
